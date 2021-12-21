f3d0c97aa39942af2ee8a6f43c01fc94cdeab1f5
antoine@thefurnitureguys.com <antoine@thefurnitureguys.ca>
2021-12-21 17:33:55 -0500

    remove parallel copy

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@
 pip install -r requirements.txt
 pip install mesh-transformer-jax/ jax==0.2.12
 pip3 install tf-nightly --no-input
-gsutil -m cp -r gs://ks-story-ew4-storage/step_383500 .
+gsutil cp -r gs://ks-story-ew4-storage/step_383500 .
 python3 gen.py
