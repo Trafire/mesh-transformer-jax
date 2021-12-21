4d0b159e89b112294c5cc82585a9559df42ebc2b
antoine@thefurnitureguys.com <antoine@thefurnitureguys.ca>
2021-12-21 16:56:02 -0500

    test multiple inference speed

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 pip install mesh-transformer-jax/ jax==0.2.12 tensorflow==2.5.0
 pip install -r requirements.txt
 gsutil cp -r gs://ks-story-ew4-storage/step_383500 .
-pip3 uninstall keras
-pip3 install keras --upgrade
-pip3 install tf-nightly
+pip3 uninstall --yes keras
+pip3 install keras --upgrade --no-input
+pip3 install tf-nightly --no-input
 python3 gen.py
