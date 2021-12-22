7ed22252663c2a067f4653a174bbcbd9a89f3fe1
antoine@thefurnitureguys.com <antoine@thefurnitureguys.ca>
2021-12-22 00:04:35 -0500

    add storage

--- main.py
+++ main.py
@@ -3,9 +3,26 @@ from gen import infer
 import store
 import os
 
-def get_prompt(bucket_name, story_name):
+def get_story(bucket_name, story_name):
     filepath =f"kindle_books/stories/{story_name}/confirmed.txt"
     return store.get_text_file(bucket_name, filepath).decode('ASCII')
+
+def find_nth(s, x, n):
+    i = -1
+    for _ in range(n):
+        i = s.find(x, i + len(x))
+        if i == -1:
+            break
+    return i
+
+
+def get_prompt(prompt, n):
+    total_words = prompt.count(" ")
+    target_index = total_words - n
+    if target_index < 1:
+        return prompt
+    index = find_nth(prompt, ' ', target_index)
+    return prompt[index:]
 
 
 bucket_name = 'ks-stories'
@@ -19,22 +36,15 @@ while True:
         json_data = store.get_json(bucket_name, json_path)
         story_name = json_data['story_name']
         version = json_data['version']
-        prompt = get_prompt(bucket_name, story_name)
+        story = get_story(bucket_name, story_name)
         for i in range(10):
-            print(prompt, type(prompt))
-
+            prompt = get_prompt(story, 250)
             data = infer(top_p=top_p, temp=temp, gen_len=512, context=prompt)
-
-            prompt += data[0]
+            print("data", data)
+            story += data[0]
             print(prompt)
             directory = f"kindle books/stories/{story_name}/drafts/version {version}/"
             filename = str(uuid.uuid4()) + ".txt"
             filepath = directory + filename
             print(f"saving to {filepath}")
-            store.write_file(bucket_name, filepath, prompt)
-
-
-
-
-
-
+            store.write_file(bucket_name, filepath, story)
